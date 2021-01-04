baab1ca3de3456f31be2bf2f35befc39ff7a3704
Danish <danishriz100@gmail.comgit commitexit>
2021-01-04 14:50:09 -0700

    mark completed and delete list

--- todo_list/README.md
+++ todo_list/README.md
@@ -22,3 +22,8 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### router and axios
+```
+npm install --save axios vue-router
+```
--- todo_list/src/App.vue
+++ todo_list/src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="bg-purple-600 text-white">
+    <div class="container mx-auto">
+      <div class="flex items-center">
+        <router-link to="/"
+                     class="inline-block px-16 py-4 transition-colors hover:bg-purple-500 duration-300">Home</router-link>
+        <router-link to="/todos"
+                     class="inline-block px-16 py-4 transition-colors hover:bg-purple-500 duration-300">Todos</router-link>
+      </div>
+    </div>
+  </nav>
+  <router-view />
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
