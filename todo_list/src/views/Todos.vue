e90f40b559c66c7484f332c95c09a1f8aa7c0c35
Danish <danishriz100@gmail.comgit commitexit>
2021-01-04 16:24:00 -0700

    Add button in todo list

--- todo_list/src/views/Todos.vue
+++ todo_list/src/views/Todos.vue
@@ -1,9 +1,26 @@
 <template>
   <div class="container mx-auto">
-    <h1 class="mt-8 text-2xl">Todos</h1>
+    <h1 class="mt-8 text-2xl text-center font-extrabold">Todos</h1>
 
     <div class="mt-3">
       <div class="grid grid-cols-12 gap-4">
+
+<!-- input box -->
+        <div class="col-span-6">
+          <div class="p-8 bg-white shadow-md rounded">
+            <h2 class="text-xl ">Add a todo</h2>
+            <input type="text"
+                   v-model="todoText"
+                   @keydown.enter="addTodo"
+                   class="p-2 mt-4 border rounded w-full">
+                  
+                    <button class="mt-5 px-2 rounded text-white bg-purple-600 hover:bg-purple-700"
+                      @click="addTodo"
+                      title="Add todo">Add</button>
+          </div>
+        </div>
+
+<!-- todo list -->
         <div class="col-span-6 space-y-4 overflow-y-auto px-1"
              style="height: 500px">
           <div v-for="(todo, index) in todos"
@@ -12,7 +29,7 @@
                :class="{'bg-green-200': todo.done}">
             <div>
               <div>{{ todo.text }}</div>
-              <div class="text-gray-500 text-sm">{{ todo.createdAt.toString() }}</div>
+              <!-- <div class="text-gray-500 text-sm">{{ todo.createdAt.toString() }}</div> -->
             </div>
             <div class="space-x-2">
               <button class="px-2 text-red-600"
@@ -30,19 +47,11 @@
           </div>
           <div v-if="todos.length === 0"
                class="px-8 py-16 bg-white text-gray-700 shadow-md rounded text-sm">
-            You dont have any task to do.
+            You dont have anything in Todo List.
           </div>
         </div>
 
-        <div class="col-span-6">
-          <div class="p-8 bg-white shadow-md rounded">
-            <h2 class="text-xl">Add a todo</h2>
-            <input type="text"
-                   v-model="todoText"
-                   @keydown.enter="addTodo"
-                   class="p-2 mt-4 border rounded w-full">
-          </div>
-        </div>
+
       </div>
     </div>
   </div>
@@ -50,10 +59,14 @@
 
 <script>
 import { defineComponent, reactive, ref } from "vue";
+
+
 export default defineComponent({
   setup() {
     const todos = reactive([]);
     const todoText = ref(""); // const todoText = {value: ""}
+
+
     function addTodo() {
       todos.unshift({
         text: todoText.value,
@@ -62,18 +75,24 @@ export default defineComponent({
       });
       todoText.value = "";
     }
+
+
     function markAsDone(index) {
       todos[index].done = true;
     }
+
+
     function markAsUndone(index) {
       todos[index].done = false;
     }
+
     function removeTodo(index) {
       if (!confirm("Are you sure?")) {
         return;
       }
       todos.splice(index, 1);
     }
+
     return {
       todos,
       todoText,
